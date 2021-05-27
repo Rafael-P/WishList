3bb0d2084b752a663a4c0baca6d2f45568a73984
rafaelgckto <rafael.gcecatto@gmail.com>
2021-05-27 10:33:35 -0300

    back-end adicionado v2

---
+++ back-end/WishList.WebApi/Domains/Desejo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WishList.WebApi.Domains
+{
+    public partial class Desejo
+    {
+        public int IdDesejos { get; set; }
+        public string Desejo1 { get; set; }
+        public string Descricao { get; set; }
+    }
+}
