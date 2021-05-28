49cf26c73bd60871abec096c2feaa027ada0db62
rafaelgckto <rafael.gcecatto@gmail.com>
2021-05-28 09:52:28 -0300

    DB adicionado

---
+++ BD/WishList_DML.sql
@@ -0,0 +1,12 @@
+--DML
+
+USE WishList
+GO
+
+INSERT INTO Desejos(Desejo, Descricao)
+VALUES			   ('exemplo', 'exemplo')
+GO
+
+INSERT INTO Usuarios(Email, Senha)
+VALUES				('adm@adm.com', '123')
+GO
